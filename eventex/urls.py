343d856fdd69deb46036e63caeb7a9dc8926a67c
euleralencar <euleralencar@gmail.com>
2014-06-10 11:19:49 -0300

    Subscription

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'eventex.core.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
+    url(r'^inscricao/(\d+)$', 'eventex.subscriptions.views.detail', name='detail'),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
